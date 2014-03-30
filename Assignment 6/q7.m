a40d4d38fe41f8bbc21fdbd038d149d57fd28485
ethan <ethan@clandestine>
2014-03-29 18:15:43 -0700

    started q7

--- Assignment 6/q7.m
+++ Assignment 6/q7.m
@@ -1,5 +1,26 @@
 function q7()
-
-
-
+    second_order(1)
 end
+
+function r = second_order(h)
+    
+
+    r = 0;
+    return
+end
+
+function r = yprime(y,t)
+    r = (t^2)-y;
+    return
+end
+
+function r = y2prime(y,t)
+    r = 0
+    return
+end
+
+function r = y3prime(y,t)
+    r = 0
+    return
+end
+
