38a55d9d48ebfc753591d86c3bfcd3d60967df1c
ethan <ethan@clandestine>
2014-03-31 20:34:25 -0700

    cleaned q7

--- Assignment 6/q7.m
+++ Assignment 6/q7.m
@@ -1,35 +1,21 @@
 function q7()
-    A = [];
     t = 0;
     h = 1;
-    x = second_order(t+h);
-    y = third_order(t+h);
+    y0 = 1;
+    x = y0 + second_order(t+h);
+    y = y0 + third_order(t+h);
     A = [2 x ((x-f(t+h))/f(t+h)); 3 y ((y-f(t+h))/f(t+h))];
     disp('    Degree    Approx    Error');
     A
 end
 
 function r = second_order(h)
-    persistent y t;
-    t = 0;
-    y = 1;
-    for i=(0+h):h:1
-        t = t + h;
-        y = y + (1 - i -((1/2)*(i^2)));
-    end
-    r = y;
+    r = (1 - h -((1/2)*(h^2)));
     return
 end
 
 function r = third_order(h)
-    persistent y t;
-    t = 0;
-    y = 1;
-    for i=(0+h):h:1
-        t = t + h;
-        y = y + (1 - i -((1/2)*(i^2)) + ((1/6)*(i^3)));
-    end
-    r = y;
+    r = (1 - h -((1/2)*(h^2)) + ((1/6)*(h^3)));
     return
 end
 
